346c93dd7977402541d18d31400e1bf9df530d57
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-04-10 10:52:26 -0500

    feat: add popup

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -1,5 +1,6 @@
 document.getElementById('focus-main-nav').focus();
 
+// burger
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('nav');
 const links = nav.querySelectorAll('a');
@@ -30,3 +31,53 @@ burger.addEventListener('click', () => {
     document.body.style.overflow = 'auto';
   }
 });
+
+//show pop-up
+const closeBtn = document.querySelector('.close-btn');
+const cards = document.querySelectorAll('.card');
+const overlay = document.querySelector('.overlay');
+const popup = document.querySelector('.popup');
+const popupTitle = document.querySelector('.popup-title');
+const popupDescription = document.querySelector('.popup-description');
+const popupImage = document.querySelector('.popup-image');
+const popupSubtitle = document.querySelector('.popup-subtitle');
+const popupList = document.querySelector('.popup-list');
+const body = document.querySelector('body');
+
+fetch('../../assets/pets.json')
+  .then(response => response.json())
+  .then(data => {
+    cards.forEach((card) => {
+      const cardId = card.id;
+
+      card.addEventListener('click', () => {
+        const cardData = data.find(card => card.id === parseInt(cardId));
+        popupTitle.textContent = cardData.name;
+        popupDescription.textContent = cardData.description;
+        popupSubtitle.textContent = cardData.type + ' - ' + cardData.breed;
+        popupImage.src = cardData.img;
+
+        popupList.innerHTML = 
+          `<li><b>Age: </b>${cardData.age}</li>
+          <li><b>Inoculations: </b>${cardData.inoculations[0]}</li>
+          <li><b>Diseases: </b>${cardData.diseases[0]}</li>
+          <li><b>Parasites: </b>${cardData.parasites[0]}</li>`
+        
+        overlay.style.display = 'flex';
+        body.classList.add('overlay-open');
+      });
+
+      overlay.addEventListener('click', (event) => {
+        if (body.classList.contains('overlay-open') && !popup.contains(event.target)) {
+            body.classList.remove('overlay-open');
+            overlay.style.display = 'none';
+        }
+      });
+
+      closeBtn.addEventListener('click', () => {
+        overlay.style.display = 'none';
+        body.classList.remove('overlay-open');
+      });
+    });
+  })
+  .catch(error => console.error(error));
