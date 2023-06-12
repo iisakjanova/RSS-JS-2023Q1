d520e563fd7928f4a11e4d24a91b2972f073f39e
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-06-13 01:32:30 +0600

    fix: add proxy API

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://rss-news-api.onrender.com/', {
       apiKey: '687b7cc7cd9f4b7e971beaafed31cb9e', // получите свой ключ https://newsapi.org/
     });
   }
