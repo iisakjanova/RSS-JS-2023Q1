2b9b36c393382ad0ae00cf47d2e29f78c02647c8
Irina Isakzhanova <irina.isakjanova@gmail.com>
2023-06-19 17:25:41 +0600

    feat: add TS, eslint configs, add prettier

---
+++ rs-css/eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    'es2022': true,
+    'node': true
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-base', 
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'prettier', 
+    '@typescript-eslint',
+  ],
+  root: true,
+  rules: {
+    'import/extentions': 'off',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'class-methods-use-this': ['error', {
+      'exceptMethods': ['errorHandler']
+    }],
+  }  
+};
